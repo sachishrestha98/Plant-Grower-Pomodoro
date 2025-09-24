886b86a00060b354fa5e74608f128695ab57ada0
sachishrestha98 <sachishrestha1998@gmail.com>
2025-09-23 21:57:19 -0400

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Sachi Shrestha on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
